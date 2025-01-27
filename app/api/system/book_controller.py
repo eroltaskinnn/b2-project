20fdbdff178895880a78778bffa52ea272d0d29f
erol taskin <eroltaskinnn@gmail.com>
2025-01-27 16:55:00 +0300

    Add try except blocks in endpoints

--- app/api/system/book_controller.py
+++ app/api/system/book_controller.py
@@ -15,29 +15,40 @@ router = APIRouter()
 
 @router.post("/books/", response_model=BookResponse)
 def create_book(book: BookCreate, db: Session = Depends(get_db)):
-    book_crud = BookCRUD(db)
-    return book_crud.create_book(book)
-
+    try:
+        book_crud = BookCRUD(db)
+        return book_crud.create_book(book)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/books/", response_model=List[BookResponse])
 def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    book_crud = BookCRUD(db)
-    return book_crud.read_books(skip, limit)
-
+    try:
+        book_crud = BookCRUD(db)
+        return book_crud.read_books(skip, limit)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/books/{book_id}", response_model=BookResponse)
 def read_book(book_id: int, db: Session = Depends(get_db)):
-    book_crud = BookCRUD(db)
-    return book_crud.read_book(book_id)
-
+    try:
+        book_crud = BookCRUD(db)
+        return book_crud.read_book(book_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.put("/books/{book_id}", response_model=BookResponse)
 def update_book(book_id: int, book: BookCreate, db: Session = Depends(get_db)):
-    book_crud = BookCRUD(db)
-    return book_crud.update_book(book_id, book)
-
+    try:
+        book_crud = BookCRUD(db)
+        return book_crud.update_book(book_id, book)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/books/{book_id}")
 def delete_book(book_id: int, db: Session = Depends(get_db)):
-    book_crud = BookCRUD(db)
-    return book_crud.delete_book(book_id)
+    try:
+        book_crud = BookCRUD(db)
+        return book_crud.delete_book(book_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/api/system/patron_controller.py
+++ app/api/system/patron_controller.py
@@ -1,7 +1,7 @@
-from typing import List, Optional
+from typing import List
 
 from fastapi import APIRouter
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import HTTPException, Depends
 from sqlalchemy.orm import  Session
 
 from core.dependency import get_db
@@ -15,29 +15,40 @@ router = APIRouter()
 # Patron Operations
 @router.post("/patrons/", response_model=PatronResponse)
 def create_patron(patron: PatronCreate, db: Session = Depends(get_db)):
-    patron_crud = PatronCRUD(db)
-    return patron_crud.create_patron(patron)
-
+    try:
+        patron_crud = PatronCRUD(db)
+        return patron_crud.create_patron(patron)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/patrons/", response_model=List[PatronResponse])
 def read_patrons(db: Session = Depends(get_db)):
-    patron_crud = PatronCRUD(db)
-    return patron_crud.read_patrons()
-
+    try:
+        patron_crud = PatronCRUD(db)
+        return patron_crud.read_patrons()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.get("/patrons/{patron_id}", response_model=PatronResponse)
 def read_patron(patron_id: int, db: Session = Depends(get_db)):
-    patron_crud = PatronCRUD(db)
-    return patron_crud.read_patron(patron_id)
-
+    try:
+        patron_crud = PatronCRUD(db)
+        return patron_crud.read_patron(patron_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.put("/patrons/{patron_id}", response_model=PatronResponse)
 def update_patron(patron_id: int, patron: PatronCreate, db: Session = Depends(get_db)):
-    patron_crud = PatronCRUD(db)
-    return patron_crud.update_patron(patron_id, patron)
-
+    try:
+        patron_crud = PatronCRUD(db)
+        return patron_crud.update_patron(patron_id, patron)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/patrons/{patron_id}")
 def delete_patron(patron_id: int, db: Session = Depends(get_db)):
-    patron_crud = PatronCRUD(db)
-    return patron_crud.delete_patron(patron_id)
+    try:
+        patron_crud = PatronCRUD(db)
+        return patron_crud.delete_patron(patron_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
